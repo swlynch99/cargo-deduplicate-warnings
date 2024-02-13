a4e8f4a4129c9fa340a8c18399cff8d80835276a
Sean Lynch <swlynch@Icarus>
2024-02-12 16:49:30 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# cargo-deduplicate-warnings
+
+This crate is a binary which deduplicates warning messages in the json output of
+`cargo`.
+
+Normally, cargo does this for you when using its normal output. However, when
+you have other tools that consume cargo's json output (e.g. `clippy-sarif`)
+then the duplicate warnings show up downstream. This crate allows you to
+avoid the issue by stripping them out.
+
+## Usage
+```bash
+cargo clippy --format json | cargo deduplicate-warnings | ...
+```
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::collections::HashSet;
+
+use cargo_metadata::Message;
+
+/// A simple cargo command to strip duplicate warnings from the json output of a
+/// cargo command.
+///
+/// Cargo json input is read on stdin and deduplicated output is emitted to
+/// stdout.
+#[derive(Clone, Debug, clap::Parser)]
+struct Args {
+    // Ignore the `deduplicate-warnings` argument if present.
+    #[arg(hide = true)]
+    subcommand: Option<String>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let stdin = std::io::stdin();
+    let mut seen = HashSet::new();
+
+    for message in Message::parse_stream(stdin.lock()) {
+        let message = message?;
+        let text = match message {
+            Message::TextLine(line) => line,
+            Message::CompilerMessage(msg) => {
+                if !seen.insert(msg.clone()) {
+                    continue;
+                }
+
+                serde_json::to_string(&Message::CompilerMessage(msg))?
+            }
+            message => serde_json::to_string(&message)?,
+        };
+
+        println!("{text}")
+    }
+
+    Ok(())
+}
